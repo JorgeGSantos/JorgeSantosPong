cea215bd5c154daa7dfca7c8b8475fd5604f2d69
Jorge Santos <jorgegoncalosantos@gmail.com>
2021-03-23 09:59:58 +0000

    update

---
+++ pong/Assets/Scripts/BOLAMOV.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOLAMOV : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 5f;
+    float tempo = 0f;
+    float esperar = 2f;
+    bool lancamento = false;
+
+    void Start()
+    {
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempo = tempo + Time.deltaTime;
+        if (lancamento == false)
+        {
+            if(tempo > esperar)
+            {
+                GetComponent<Rigidbody2D>().velocity = velocidade * Random.onUnitSphere;
+                lancamento = true;
+            }
+        }
+    }
+}
---
+++ pong/Assets/Scripts/PADDLELEFT.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PADDLELEFT : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 0.5f;
+
+    [SerializeField]
+    float paddlemov = 1f;
+
+    [SerializeField]
+    KeyCode upKey = KeyCode.UpArrow;
+
+    [SerializeField]
+    KeyCode downKey = KeyCode.DownArrow;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(upKey))
+        {
+            transform.position += velocidade * Vector3.up * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(downKey))
+        {
+            transform.position += velocidade * Vector3.down * Time.deltaTime;
+        }
+        float CAMARAHEIGHT = Camera.main.orthographicSize;
+        Vector3 positionAux = transform.position;
+        positionAux.y = Mathf.Clamp(positionAux.y, -CAMARAHEIGHT + (paddlemov / 2), CAMARAHEIGHT - (paddlemov / 2));
+        transform.position = positionAux;
+    }
+}
